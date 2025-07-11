809f945b3c333d55ae7b428281c0398c953b0bf0
KhoiMASE203058 <maikhoistudent@gmail.com>
2025-07-11 22:21:25 +0700

    Update Student.java

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,37 +1,17 @@
 package model;
 
-public class Student {
-    private int id;
-    private String fullName;
-    private double gpa;
+public class student {
+    public int id;
+    public String name;
+    public double gpa;
 
-    public Student(int id, String fullName, double gpa) {
+    public student(int id, String name, double gpa) {
         this.id = id;
-        this.fullName = fullName;
+        this.name = name;
         this.gpa = gpa;
     }
 
-    public int getId() { 
-        return id; 
-    }
-
-    public String getFullName() { 
-        return fullName; 
-    }
-    public double getGpa() { 
-        return gpa; 
-    }
-
-    public void setFullName(String fullName) { 
-        this.fullName = fullName; 
-    }
-
-    public void setGpa(double gpa) { 
-        this.gpa = gpa; 
-    }
-
-    @Override
     public String toString() {
-        return String.format("%-10d %-30s %.2f", id, fullName, gpa);
+        return id + " - " + name + " - " + gpa;
     }
 }
