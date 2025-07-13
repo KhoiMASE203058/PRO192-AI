05439b6e65c2d92d9d687040643bc635f617228a
KhoiMASE203058 <maikhoistudent@gmail.com>
2025-07-13 20:12:03 +0700

    Update StudentService.java

--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -1,62 +1,40 @@
 package service;
 
-import model.Student;
-import util.Validator;
+import model.student;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
-public class StudentService {
+public class studentservice {
+    public ArrayList<student> list = new ArrayList<>();
 
-    private final List<Student> students = new ArrayList<>();
-
-    public void addStudent(Student s) throws Exception {
-        if (students.size() >= 100) {
-            throw new Exception("Maximum student limit reached.");
-        }
-
-        if (!Validator.isValidGPA(s.getGpa())) {
-            throw new Exception("Invalid GPA.");
-        }
-
-        if (getStudentById(s.getId()) != null) {
-            throw new Exception("Student ID already exists.");
-        }
-
-        students.add(s);
+    public void add(student s) {
+        list.add(s);
     }
 
-    public void deleteStudent(int id) throws Exception {
-        Student s = getStudentById(id);
-        if (s == null) {
-            throw new Exception("Student not found.");
-        }
-        students.remove(s);
-    }
+    public void delete(int id) {
+        for (student s : list) {
+            if (s.id == id) {
+                try {
+                    list.remove(s);
+                } catch (Exception e) {
 
-    public List<Student> searchStudent(String name) {
-        List<Student> result = new ArrayList<>();
-        for (Student s : students) {
-            if (s.getFullName().toLowerCase().contains(name.toLowerCase())) {
-                result.add(s);
+                }
+                break;
             }
         }
-        return result;
     }
 
-    public void displayStudents() {
+    public void find(String name) {
+        for (student s : list) {
+            if (s.name.toLowerCase() == name.toLowerCase()) {
+                System.out.println(s);
+            }
+        }
+    }
 
-        System.out.printf("%-10s %-30s %s\n", "ID", "Full Name", "GPA");
-
-        for (Student s : students) {
+    public void display() {
+        for (student s : list) {
             System.out.println(s);
         }
-    }
-
-    private Student getStudentById(int id) {
-
-        for (Student s : students) {
-            if (s.getId() == id) return s;
-        }
-        return null;
     }
 }
